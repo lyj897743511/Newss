46df95f2dede97e12cc980d11e95f4d0136807ab
eclipse_user <eclipse_liuyangjun@qq.com>
2019-02-20 15:22:59 +0800

    commit happy

---
+++ src/main/java/com/liuyangjun/git/Happy.java
@@ -0,0 +1,19 @@
+/**
+ *  Copyright (c)  2018-2028 DHCC, Inc.
+ *  All rights reserved.
+ *
+ *  This software is the confidential and proprietary information of DHCC, 
+ *  Inc. ("Confidential Information"). You shall not
+ *  disclose such Confidential Information and shall use it only in
+ *  accordance with the terms of the license agreement you entered into with DHCC.
+ */
+package com.liuyangjun.git;
+
+/**
+ * 
+ * @author 刘洋君
+ * @date 2019年2月20日
+ */
+public class Happy {
+
+}
